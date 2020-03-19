ecca517307ca4bfa427fcf083696081236467fff
billsjchw <billsjc@126.com>
2020-03-19 22:41:39 +0800

    Implement BookCard

---
+++ src/books.js
@@ -0,0 +1,11 @@
+const books = [
+    {
+        id: 1,
+        title: "Thinking in Java (Fourth Edition)",
+        author: "Bruce Eckel",
+        price: 518.00,
+        image: "1.jpg"
+    }
+];
+
+export default books;
---
+++ src/components/BookCard/BookCard.js
@@ -0,0 +1,25 @@
+import React from "react";
+import '../../bootstrap.css'
+import './BookCard.css'
+import {Card} from "react-bootstrap";
+
+class BookCard extends React.Component {
+    render() {
+        return (
+            <Card className="book-card">
+                <div className="d-flex justify-content-center pt-3 pl-3 pr-3">
+                    <img src={require("../../images/books/" + this.props.book.image)} alt={this.props.book.title}/>
+                </div>
+                <Card.Body>
+                    <Card.Text className="text-ellipsis">
+                        {this.props.book.title}<br/>
+                        {this.props.book.author}<br/>
+                        &yen;{this.props.book.price.toFixed(2)}
+                    </Card.Text>
+                </Card.Body>
+            </Card>
+        );
+    }
+}
+
+export default BookCard;
